1fc3a6c7d57362cdb9a99f0f0133960858343d11
Isabela Rodrigues <154336126+belarod@users.noreply.github.com>
2024-10-23 11:23:12 -0300

    correção atv 23-10

--- atv-23-10-24-NS/employee_department.sql
+++ atv-23-10-24-NS/employee_department.sql
@@ -1,15 +1,15 @@
-SELECT *
+SELECT * --employees.employeename AS nome_empregado, departments.dptoname AS nome_departamento
 FROM employees
 LEFT JOIN departments ON employees.dptoid = departments.dptoid;
 
 --
 
-SELECT *
+SELECT * --employees.employeename AS nome_empregado, departments.dptoname AS nome_departamento
 FROM employees
 INNER JOIN departments ON employees.dptoid = departments.dptoid;
 
 --
 
-SELECT DISTINCT *
+SELECT DISTINCT * --departments.dptoname AS nome_departamento (distinct p/ nao duplicar os nomes)
 FROM departments
 INNER JOIN employees ON departments.dptoid = employees.dptoid;
